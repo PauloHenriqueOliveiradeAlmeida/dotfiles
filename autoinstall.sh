0ddb193e808e0e0599e3f4e048b7ad2ec9a27c54
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2024-07-14 17:34:35 -0300

    :sparkles: feat: added starship

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sudo pacman -Syu polybar neovim kitty fish rofi picom ly ttf-firacode-nerd code-oss
+sudo pacman -Syu polybar neovim kitty fish rofi picom ly ttf-firacode-nerd code starship &&
 
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-omf install https://github.com/dracula/fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install &&
+omf install https://github.com/dracula/fish &&
 
-sudo systemctl disable lightdm.service
-sudo systemctl enable ly.service
+sudo systemctl disable lightdm.service &&
+sudo systemctl enable ly.service &&
 
-cp ./polybar /home/$USER/.config/ -r
-cp ./i3 /home/$USER/.config/ -r
-cp ./nvim /home/$USER/.config/ -r
-cp ./kitty /home/$USER/.config/ -r
-cp ./fish /home/$USER/.config/ -r
-cp ./rofi /home/$USER/.config/ -r
-cp ./picom/picom.conf /home/$USER/.config/picom.conf -r
-cp ./omf /home/$USER/.config -r
-sudo cp ./ly/config.ini /etc/ly -r
+cp ./polybar /home/$USER/.config/ -r &&
+cp ./i3 /home/$USER/.config/ -r &&
+cp ./nvim /home/$USER/.config/ -r &&
+cp ./kitty /home/$USER/.config/ -r &&
+cp ./fish /home/$USER/.config/ -r &&
+cp ./rofi /home/$USER/.config/ -r &&
+cp ./picom/picom.conf /home/$USER/.config/picom.conf -r &&
+cp ./omf /home/$USER/.config -r &&
+sudo cp ./ly/config.ini /etc/ly -r &&
 
-sudo reboot
+echo "OK - reboot system now"
